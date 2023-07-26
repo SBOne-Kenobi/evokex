b110a7ef8ed9311179f4cfccb6fe786804650a66
Rustam Sadykov <sbonekenobi@gmail.com>
2023-07-26 15:39:38 +0200

    add test generator

--- client/src/main/kotlin/org/evosuite/kex/Util.kt
+++ client/src/main/kotlin/org/evosuite/kex/Util.kt
@@ -6,8 +6,11 @@ import kotlinx.serialization.InternalSerializationApi
 import org.evosuite.runtime.RuntimeSettings
 import org.evosuite.runtime.sandbox.Sandbox
 import org.vorpal.research.kex.ExecutionContext
+import org.vorpal.research.kex.asm.analysis.util.checkAsync
+import org.vorpal.research.kex.descriptor.Descriptor
 import org.vorpal.research.kex.ktype.KexRtManager.isJavaRt
 import org.vorpal.research.kex.ktype.KexRtManager.rtMapped
+import org.vorpal.research.kex.parameters.Parameters
 import org.vorpal.research.kex.smt.AsyncChecker
 import org.vorpal.research.kex.smt.Result
 import org.vorpal.research.kex.smt.SMTModel
@@ -16,15 +19,23 @@ import org.vorpal.research.kex.trace.symbolic.SymbolicState
 import org.vorpal.research.kfg.ir.Method
 import kotlin.time.ExperimentalTime
 
+private inline fun <T> withoutSandbox(block: () -> T): T {
+    // FIXME: do smth else here with security manager
+    val mode = RuntimeSettings.sandboxMode
+    RuntimeSettings.sandboxMode = Sandbox.SandboxMode.OFF
+    return try {
+        block()
+    } finally {
+        RuntimeSettings.sandboxMode = mode
+    }
+}
+
 @ExperimentalTime
 @InternalSerializationApi
 @ExperimentalSerializationApi
 @DelicateCoroutinesApi
-suspend fun SymbolicState.check(ctx: ExecutionContext, method: Method = KexService.fakeEmptyMethod): SMTModel? {
-    // FIXME: do smth else here with security manager
-    val mode = RuntimeSettings.sandboxMode
-    RuntimeSettings.sandboxMode = Sandbox.SandboxMode.OFF
-    try {
+suspend fun SymbolicState.check(ctx: ExecutionContext, method: Method = KexService.fakeEmptyMethod): SMTModel? =
+    withoutSandbox {
         val checker = AsyncChecker(method, ctx)
         val clauses = clauses.asState()
         val query = path.asState()
@@ -34,8 +45,10 @@ suspend fun SymbolicState.check(ctx: ExecutionContext, method: Method = KexServi
             .mapValues { it.value.rtMapped }
             .toTypeMap()
         val result = checker.prepareAndCheck(method, clauses + query, concreteTypeInfo, enableInlining = true)
-        return (result as? Result.SatResult)?.model
-    } finally {
-        RuntimeSettings.sandboxMode = mode
+        (result as? Result.SatResult)?.model
     }
-}
+
+suspend fun SymbolicState.checkAndGetParameters(ctx: ExecutionContext, method: Method): Parameters<Descriptor>? =
+    withoutSandbox {
+        method.checkAsync(ctx, this, enableInlining = true)
+    }
